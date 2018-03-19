137c8a2f562da825e3a9f3026713ba4378407700
Jacob-Ayers <37543253+Jacob-Ayers@users.noreply.github.com>
2018-03-19 11:25:19 -0400

    Added name input

--- hello.java
+++ hello.java
@@ -1,15 +1,15 @@
 // a simple program to practice common Github commands 
-
+import java.util.Scanner;
 public class hello {
 
     public static void main(String [] args) {
+        Scanner.NameInput = new Scanner(System.in);
+        String name;
 
-
-	
-	// added  line	
-	// added another line
-
-        System.out.println("Hello Class!");
+        System.out.println("Please enter your name: ");
+        name = NameInput.next();
+        System.out.println("Hello " + name + ", my name is Jacob");
+        
 
 
 
